7018af759628b5f6eba5fbe133ab10d7ce15414d
iamjohn <john@gmail.com>
2017-07-20 22:39:09 +0300

    I think that this is the end

--- my.js
+++ my.js
@@ -1,1 +1,13 @@
 
+const list = $('.tabs li');
+
+const tabsContent = $('.tabs-content');
+
+list.on('click', function(){
+   list.css('border-bottom','3px solid #cdcdcd');
+   $(this).css('border-bottom','3px solid #efb93f');
+   $(this).css('color','rgba(0,0,0,0.9)');
+
+   tabsContent.fadeOut(300).fadeIn(300);
+
+})

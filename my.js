8159906f4758189b8f9b637de5bcae10e221cd52
iamjohn <john@gmail.com>
2017-07-20 23:20:02 +0300

    Added some shadows and filters. Done

--- my.js
+++ my.js
@@ -6,6 +6,7 @@ const tabsContent = $('.tabs-content');
 list.on('click', function(){
    list.css('border-bottom','3px solid #cdcdcd');
    $(this).css('border-bottom','3px solid #efb93f');
+   list.css('color','rgba(0,0,0,0.5)');
    $(this).css('color','rgba(0,0,0,0.9)');
 
    tabsContent.fadeOut(300).fadeIn(300);

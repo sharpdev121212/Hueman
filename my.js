2a8141137553e3d5bf038496fb71474b12695f67
iamjohn <john@gmail.com>
2017-07-21 16:59:47 +0300

    Added some js based responsiveness

--- my.js
+++ my.js
@@ -12,3 +12,32 @@ list.on('click', function(){
    tabsContent.fadeOut(300).fadeIn(300);
 
 })
+
+
+$('.left a').on('click', function(e){
+   e.preventDefault();
+});
+
+$(window).resize(rsz);
+
+rsz();
+
+function rsz(){
+   const windowWidth = $(window).width();
+   if(windowWidth<1000){
+      $(".blog,.bottomheader>:nth-child(2)").css('width','70%');
+      $('.multiselect,.bottomheader>:nth-child(3)').addClass("none");
+      $('.discover,.bottomheader>:nth-child(1)').removeClass('none').css('width',"30%");
+
+   }
+   if(windowWidth<850){
+      $(".blog,.bottomheader>:nth-child(2)").css('width','100%');
+      $('.discover,.bottomheader>:nth-child(1)').addClass('none');
+      return;
+   }
+   if(windowWidth>1000){
+      $(".blog,.bottomheader>:nth-child(2)").css('width','60%');
+      $('.discover,.bottomheader>:nth-child(1)').css('width',"23%");
+      $('.none').removeClass('none');
+   }
+}
